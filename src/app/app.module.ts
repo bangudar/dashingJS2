2d7aeb99a2c947e746a77645055b1f901695bca2
pedro <charabi.badr@gmail.com>
2018-05-28 12:05:20 +0000

    Sass 1 : tuto 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LolComponent } from './lol/lol.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LolComponent
   ],
   imports: [
     BrowserModule
